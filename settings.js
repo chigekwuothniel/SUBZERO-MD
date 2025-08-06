9d741f1e9303b1eb803a88a9210bc5be11b88683
chigekwuothniel <74152397+chigekwuothniel@users.noreply.github.com>
2025-08-06 20:08:56 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;yFVBwR7T#WmwdDUeAGDQSeDtdiTmaRpjzCQWTRZDlpX7A2mOp_CE", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;2BNARB7R#7W35u3fu-ZkSuLSbumZFqqlL-tv092XPzaGncFg139c", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
